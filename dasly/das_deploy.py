a30477155595f3d2d991a5542458b3dd6640aa6d
truongphanduykhanh <truongphanduykhanh@gmail.com>
2024-08-06 12:14:41 +0200

    deploy

--- dasly/das_deploy.py
+++ dasly/das_deploy.py
@@ -375,30 +375,30 @@ class MyHandler(FileSystemEventHandler):
                 run_dasly(event.src_path)
                 self.event_count = 0  # Reset the event count
 
-    # def on_moved(self, event):
-    #     """Event handler for file moving. In integrator, when a hdf5 file is
-    #     completely written, it is moved from hdf5.tmp to hdf5.
-    #     """
-    #     if (
-    #         # Check if the event is a hdf5 file
-    #         event.dest_path.endswith('.hdf5') and
-    #         # Ensure the file is not a duplicate (sometimes watchdog triggers
-    #         # the created event twice for the same file. This should be a bug
-    #         # and we need to work around it by storing the last created file)
-    #         event.dest_path != self.last_created
-    #     ):
-    #         time.sleep(1)  # Wait for the file to be completely written
-    #         logger.info(f'New hdf5: {event.dest_path}')
-    #         self.last_created = event.dest_path  # Update the last created
-    #         # In case we set the batch more than 10 seconds (i.e. wait for
-    #         # more than one file to be created before running dasly), we need
-    #         # to count the number of events and run dasly only when the event
-    #         # count reaches the threshold
-    #         self.event_count += 1
-    #         if self.event_count >= self.event_thresh:
-    #             logger.info('Runing dasly...')
-    #             run_dasly(event.dest_path)
-    #             self.event_count = 0  # Reset the event count
+    def on_moved(self, event):
+        """Event handler for file moving. In integrator, when a hdf5 file is
+        completely written, it is moved from hdf5.tmp to hdf5.
+        """
+        if (
+            # Check if the event is a hdf5 file
+            event.dest_path.endswith('.hdf5') and
+            # Ensure the file is not a duplicate (sometimes watchdog triggers
+            # the created event twice for the same file. This should be a bug
+            # and we need to work around it by storing the last created file)
+            event.dest_path != self.last_created
+        ):
+            time.sleep(1)  # Wait for the file to be completely written
+            logger.info(f'New hdf5: {event.dest_path}')
+            self.last_created = event.dest_path  # Update the last created
+            # In case we set the batch more than 10 seconds (i.e. wait for
+            # more than one file to be created before running dasly), we need
+            # to count the number of events and run dasly only when the event
+            # count reaches the threshold
+            self.event_count += 1
+            if self.event_count >= self.event_thresh:
+                logger.info('Runing dasly...')
+                run_dasly(event.dest_path)
+                self.event_count = 0  # Reset the event count
 
 
 # Initialize the Observer and EventHandler
